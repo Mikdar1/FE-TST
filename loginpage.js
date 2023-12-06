cddc0288962668f8cf76a065ad2a957809c22365
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2023-12-06 23:15:17 +0700

    Add files via upload

---
+++ loginpage.js
@@ -0,0 +1,65 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const switchers = [...document.querySelectorAll('.switcher')];
+  
+    switchers.forEach(item => {
+      item.addEventListener('click', function () {
+        switchers.forEach(item => item.parentElement.classList.remove('is-active'));
+        this.parentElement.classList.add('is-active');
+      });
+    });
+    
+    const loginForm = document.querySelector('.form-login');
+    const loginButton = document.getElementById('loginButton');
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault(); // Prevent the default form submission behavior
+    
+      // Get the values from the form
+      const username = document.getElementById('login-username').value;
+      const password = document.getElementById('login-password').value;
+      loginButton.classList.add('btn-login-clicked');
+      const apiUrl = 'http://tugasghaylan.a9gec8gtbgekdqcz.southeastasia.azurecontainer.io/token';
+      
+    
+      // Make the API request using Fetch API
+      
+      const formData = new URLSearchParams();
+      formData.append('grant_type', 'password');
+      formData.append('username', username);
+      formData.append('password', password);
+      
+      const response = await fetch(apiUrl, {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: formData
+      });
+      
+      // Rest of your code...
+      
+    
+        if (response.ok) {
+            window.location.href = "http://tugasghaylan.a9gec8gtbgekdqcz.southeastasia.azurecontainer.io/docs";
+        } else {
+            try {
+                const errorData = await response.json();
+                console.log('Error Response:', errorData);
+                
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+                
+            }
+            finally {
+              // Remove the 'btn-login-clicked' class to revert the button color
+              loginButton.classList.remove('btn-login-clicked');
+          }
+        }
+        
+    });
+  });
+  
+
+
