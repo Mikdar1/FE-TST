c1a68b8216d3ceff7e5ed10cb75e9f4f5c9795a6
Ghaylan <102856179+Mikdar1@users.noreply.github.com>
2023-12-08 20:30:45 +0700

    final ver

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const username = document.getElementById('login-username').value;
       const password = document.getElementById('login-password').value;
       loginButton.classList.add('btn-login-clicked');
-      const apiUrl = 'https://healthcaretst.livelycliff-504ee3e2.southeastasia.azurecontainerapps.io/token';
+      const apiUrl = 'https://tsthealthcare.livelycliff-504ee3e2.southeastasia.azurecontainerapps.io/token';
       
     
       // Make the API request using Fetch API
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
       
     
         if (response.ok) {
-            window.location.href = "https://healthcaretst.livelycliff-504ee3e2.southeastasia.azurecontainerapps.io/docs";
+            window.location.href = "https://tsthealthcare.livelycliff-504ee3e2.southeastasia.azurecontainerapps.io/docs";
         } else {
             try {
                 const errorData = await response.json();
